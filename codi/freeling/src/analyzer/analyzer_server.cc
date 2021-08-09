b3eed6046ebe4b066a2199554a9181d8091d8e50
guillempla <guillem.pla@est.fib.upc.edu>
2021-08-09 17:42:31 +0200

    Moved directories

--- codi/freeling/src/analyzer/analyzer_server.cc
+++
@@ -1,8 +0,0 @@
-
-#include "analyzer_pool.h"
-#define WORKER analyzer_pool
-
-/// tell the handler whether to include stats also in response for final user
-const bool UserStats=true;
-
-#include "server.cc"
