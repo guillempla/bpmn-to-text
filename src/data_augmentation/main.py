d49cd2481bd5c38682ab6ef097f9c4bfbba5a738
guillempla <guillem.pla@est.fib.upc.edu>
2021-08-09 19:01:50 +0200

    Updated main

---
+++ src/data_augmentation/main.py
@@ -0,0 +1,35 @@
+import os
+from pathlib import Path
+import xml.etree.ElementTree as ET
+
+DATA_PATH = "../../bpmn"
+
+
+def read_bpmn_files():
+    """
+    Finds all BPMN files inside the DATA_PATH, parses it to an ElementTree and stores it to an
+    array of ElementTrees.
+
+    :return:
+    tree_array: Array of XML Trees, each position contains the Tree representation of an BPMN.
+    """
+
+    tree_array = []
+
+    for file in Path(DATA_PATH).glob('**/*.bpmn'):
+        tree = ET.parse(os.fspath(file))
+        tree_array.append(tree)
+
+    return tree_array
+
+
+def main():
+    parsed_files = read_bpmn_files()
+
+    for tree in parsed_files:
+        for child in tree.getroot():
+            print(child.tag)
+
+
+if __name__ == "__main__":
+    main()
