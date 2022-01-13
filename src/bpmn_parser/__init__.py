e8e923f3483fc8a06dd27a7d66524ffb157b7787
guillempla <guillem.pla@est.fib.upc.edu>
2022-01-13 20:05:21 +0100

    Restructured project

--- src/bpmn_parser/__init__.py
+++
@@ -1,3 +0,0 @@
-from .bpmn_model import BpmnModel
-from .user_form_message import UserFormMessage
-from .bpmn_types import *
--- src/bpmn_parser/user_form_message.py
+++
@@ -1,6 +0,0 @@
-class UserFormMessage:
-    def __init__(self, taskid, form_data=None):
-        if form_data is None:
-            form_data = {}
-        self.taskid = taskid
-        self.form_data = form_data
