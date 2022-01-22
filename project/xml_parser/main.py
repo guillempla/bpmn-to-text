144c0bf0c9937d6f0e5c0fa331bbac6c34f96a8c
guillempla <guillem.pla@est.fib.upc.edu>
2022-01-22 18:46:47 +0100

    Updated main

--- project/xml_parser/main.py
+++ project/xml_parser/main.py
@@ -1,16 +1,25 @@
 # Python code to parse a BPMN file
+import logging
 import xml.etree.ElementTree as ET
-from Elements import Event, Flow, Gateway, Process
+from Elements import Event, Flow, Gateway, Process, LaneSet
+import os
+import pickle
+from pathlib import Path
+
+# import untangle
+
+BPMN_PATH = "../bpmn_models"
+PICKLE_PATH = "../parsed_bpmn/"
 
 
-def parseXML(xmlfile):
+def parse_xml(xml_file):
 	# create element tree object
-	tree = ET.parse(xmlfile)
+	tree = ET.parse(xml_file)
 
 	# get root element
 	root = tree.getroot()
-	print(root.tag)
-	print()
+	logging.debug(root.tag)
+	logging.debug("\n")
 
 	processes = []
 
@@ -19,7 +28,7 @@ def parseXML(xmlfile):
 		if item.tag.__contains__('process'):
 
 			p = Process(item.attrib['id'])
-			print("     Process:    ", p.getId())
+			logging.debug("     Process:    ", p.getId())
 
 			# empty events, flows dictionary
 			events = []
@@ -28,44 +37,88 @@ def parseXML(xmlfile):
 
 			# iterate child elements of item
 			for child in item:
+				try:
+					child.attrib['id']
+				except KeyError:
+					continue
 				if child.tag.__contains__('sequenceFlow'):
 					f = Flow(child.attrib['id'], child.attrib['sourceRef'], child.attrib['targetRef'])
-					print("         Sequence Flow: ", f.getId())
-					print("         Incoming:      ", f.getIncoming())
-					print("         Outcoming:     ", f.getOutgoing())
+					logging.debug("         Sequence Flow: ", f.getId())
+					logging.debug("         Incoming:      ", f.getIncoming())
+					logging.debug("         Outcoming:     ", f.getOutgoing())
 					flows.append(f)
 				elif child.tag.__contains__('exclusiveGateway'):
-					g = Gateway(child.attrib['id'], child.attrib['name'], child.attrib['gatewayDirection'])
-					print("         Gateway:       ", g.getId())
-					print("         Name:          ", g.getName())
-					print("         Direction:     ", g.getGatewayDirection())
+					try:
+						name = child.attrib['name']
+					except KeyError:
+						name = "NA"
+					try:
+						direction = child.attrib['gatewayDirection']
+					except KeyError:
+						direction = "NA"
+					g = Gateway(child.attrib['id'], name, direction)
+					logging.debug("         Gateway:       ", g.getId())
+					logging.debug("         Name:          ", g.getName())
+					logging.debug("         Direction:     ", g.getGatewayDirection())
 					gateways.append(g)
+				elif child.tag.__contains__('laneSet'):
+					try:
+						name = child.attrib['name']
+					except KeyError:
+						name = "NA"
+					l = LaneSet(child.attrib['id'], name)
+					logging.debug("         LaneSet:       ", l.getId())
+					logging.debug("         Name:          ", l.getName())
 				else:
-					e = Event(child.attrib['id'], child.attrib['name'])
-					print("         Event:         ", e.getId())
-					print("         Name:          ", e.getName())
+					try:
+						name = child.attrib['name']
+					except KeyError:
+						name = "NA"
+					e = Event(child.attrib['id'], name)
+					logging.debug("         Event:         ", e.getId())
+					logging.debug("         Name:          ", e.getName())
 					events.append(e)
 
-				print()
+				logging.debug("\n")
 
 			p.setEvents(events)
 			p.setGateways(gateways)
 			p.setFlows(flows)
 			processes.append(p)
-			print()
+			logging.debug("\n")
 
 	# return BPMN elements list
 	return processes
 
 
+def save_model_as_pickle(file_name, model):
+	name_file = PICKLE_PATH + file_name + '.txt'
+	with open(name_file, 'wb') as fh:
+		pickle.dump(model, fh)
+
+
+def read_bpmn_files():
+	"""
+	Finds all BPMN files inside the DATA_PATH, parses it and stores it to an array of BpmnModel.
+
+	:return:
+	bpmn_array: Array of BpmnModel objects, each position contains the BPMN parsed into an BpmnModel object.
+	"""
+
+	bpmn_array = []
+
+	for file in Path(BPMN_PATH).glob('**/*.bpmn'):
+		print(file.name)
+		model = parse_xml(os.fspath(file))
+		# save_model_as_pickle(file.name, model)
+		bpmn_array.append(model)
+
+	return bpmn_array
+
+
 def main():
-	# model_path of BPMN model
-	model_path = '../bpmn_models/A20/A.2.0.bpmn'
-
-	# parse xml file
-	model_parsed = parseXML(model_path)
-
-	print(model_parsed)
+	# parse all bpmn files
+	models_parsed = read_bpmn_files()
 
 
 if __name__ == "__main__":
