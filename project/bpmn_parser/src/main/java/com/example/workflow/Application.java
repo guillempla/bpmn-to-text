76d502f2cac82967c3de97ec18093631d5d8fe79
guillempla <guillem.pla@est.fib.upc.edu>
2022-02-07 19:08:43 +0100

    Added loop to parse all the models

--- project/bpmn_parser/src/main/java/com/example/workflow/Application.java
+++ project/bpmn_parser/src/main/java/com/example/workflow/Application.java
@@ -13,10 +13,45 @@ import java.util.Collection;
 
 public class Application {
 
-  public static void main(String... args) {
+  public static void main(String[] args) {
+    String models_path = "../bpmn_models/";
+    if (args.length > 0) {
+      models_path = args[0];
+    }
 
+    ArrayList<String> bpmn_paths = getBpmnPaths(models_path);
+    for (String path: bpmn_paths) {
+      readModelFromFile(path);
+    }
+  }
+
+  public static ArrayList<String> getBpmnPaths(String path) {
+    ArrayList<String> paths = new ArrayList<>();
+
+    File directoryPath = new File(path);
+    //List of all files and directories
+    File[] filesList = directoryPath.listFiles();
+    if (filesList != null) {
+      for (File file : filesList) {
+
+        File filePath = new File(file.getAbsolutePath());
+        //List of all files and directories
+        String[] contents = filePath.list();
+        if (contents != null) {
+          for (String content : contents) {
+            if (content.contains(".bpmn")) {
+              paths.add(file.getAbsolutePath()+"/"+content);
+            }
+          }
+        }
+      }
+    }
+    return paths;
+  }
+
+  public static void readModelFromFile(String path) {
     // Read a model from a file
-    File file = new File("/home/guillem/Documents/upc/TFG/project/bpmn_models/A20/A.2.0.bpmn");
+    File file = new File(path);
     BpmnModelInstance modelInstance = Bpmn.readModelFromFile(file);
 
     // Find all elements of the type Task
@@ -31,7 +66,7 @@ public class Application {
     for (ModelElementInstance startInstance : startInstances) {
       StartEvent start = (StartEvent) startInstance;
       System.out.println(start.getName());
-      Collection<SequenceFlow> sequenceFlow = (Collection<SequenceFlow>) start.getOutgoing();
+      Collection<SequenceFlow> sequenceFlow = start.getOutgoing();
       for (SequenceFlow flow : sequenceFlow) {
         FlowNode source = flow.getTarget();
         System.out.println(source.getName());
