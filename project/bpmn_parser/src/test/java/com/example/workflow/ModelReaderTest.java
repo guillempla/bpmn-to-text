67cf0f661e290b175d60579fe6d8f5c5b9f258f7
guillempla <guillem.pla@est.fib.upc.edu>
2022-02-16 21:17:03 +0100

    Updated ModelReaderTest

--- project/bpmn_parser/src/test/java/com/example/workflow/ModelReaderTest.java
+++ project/bpmn_parser/src/test/java/com/example/workflow/ModelReaderTest.java
@@ -1,6 +1,10 @@
 package com.example.workflow;
 
 import junit.framework.TestCase;
+import org.springframework.ui.Model;
+
+import java.util.Arrays;
+import java.util.Map;
 
 public class ModelReaderTest extends TestCase {
 
@@ -14,5 +18,13 @@ public class ModelReaderTest extends TestCase {
     }
 
     public void testGetAllFlowNodes() {
+    }
+
+    public void testGetPoolsFromModel() {
+//        int[] ids = {36, 65, 73, 1235, 532};
+//        pools.put("Customer", ids);
+//        for(Map.Entry<String, int[]> m: pools.entrySet()){
+//            System.out.println(m.getKey()+" "+ Arrays.toString(m.getValue()));
+//        }
     }
 }
