dd8acc8ca760fe54ac3f45ddb8bc718728e1d3b2
guillempla <guillem.pla@est.fib.upc.edu>
2022-02-16 20:14:22 +0100

    Added Tests

---
+++ project/bpmn_parser/src/test/java/com/example/workflow/ModelReaderTest.java
@@ -0,0 +1,18 @@
+package com.example.workflow;
+
+import junit.framework.TestCase;
+
+public class ModelReaderTest extends TestCase {
+
+    public void testReadModelFromFile() {
+    }
+
+    public void testGetElementsOfType() {
+    }
+
+    public void testGetFlowingFlowNodes() {
+    }
+
+    public void testGetAllFlowNodes() {
+    }
+}
