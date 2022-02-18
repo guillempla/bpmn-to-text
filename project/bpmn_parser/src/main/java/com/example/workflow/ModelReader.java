08b4dd960c43024e12abb3aea17154f4c32490a6
guillempla <guillem.pla@est.fib.upc.edu>
2022-02-18 18:02:44 +0100

    Save all elements in BPMN

--- project/bpmn_parser/src/main/java/com/example/workflow/ModelReader.java
+++ project/bpmn_parser/src/main/java/com/example/workflow/ModelReader.java
@@ -13,17 +13,17 @@ public class ModelReader {
     String path;
     File file;
     BpmnModelInstance modelInstance;
-    ArrayList<ModelElementInstance> read_elements;
+    Map<String, ModelElementInstance> elements;
     Map<String, ArrayList<String>> lanes;
 
     public ModelReader(String path) {
         this.path = path;
         this.file = new File(path);
         this.modelInstance = Bpmn.readModelFromFile(file);
-        this.read_elements = new ArrayList<>();
+        this.elements = new HashMap<>();
         this.lanes = new HashMap<>();
         this.saveLanesFromModel();
-        System.out.println(lanes.size());
+        this.saveElementsFromModel();
     }
 
     public void saveLanesFromModel() {
@@ -41,51 +41,39 @@ public class ModelReader {
         }
     }
 
-    public void getElementsFromModel() {
-        // Find all elements of the type Task
-        Collection<ModelElementInstance> taskInstances = getElementsOfType(Task.class);
-        for (ModelElementInstance taskInstance : taskInstances) {
-            System.out.println(taskInstance.getAttributeValue("name"));
-        }
-        System.out.println();
-
+    public void saveElementsFromModel() {
         // Find all elements of type Start Event
         Collection<ModelElementInstance> startInstances = getElementsOfType(StartEvent.class);
         for (ModelElementInstance startInstance : startInstances) {
-            StartEvent start = (StartEvent) startInstance;
-            System.out.println(start.getName());
-            Collection<SequenceFlow> sequenceFlow = start.getOutgoing();
-            for (SequenceFlow flow : sequenceFlow) {
-                FlowNode source = flow.getTarget();
-                System.out.println(source.getName());
+            FlowNode start = (FlowNode) startInstance;
+            if (addElement(startInstance)) {
+                saveFollowingElements(start);
             }
         }
         System.out.println();
-
-        ModelElementType flowNode = modelInstance.getModel().getType(FlowNode.class);
-        System.out.println(flowNode);
     }
 
-    public Collection<ModelElementInstance> getElementsOfType(Class c) {
+    public void saveFollowingElements(FlowNode node) {
+        for (SequenceFlow sequenceFlow : node.getOutgoing()) {
+            FlowNode next = sequenceFlow.getTarget();
+            if (addElement(next)) {
+                saveFollowingElements(next);
+            }
+        }
+    }
+
+    public boolean addElement(ModelElementInstance instance) {
+        String instance_id = instance.getAttributeValue("id");
+        if (!elements.containsKey(instance_id)) {
+            elements.put(instance_id, instance);
+            System.out.println(instance_id + " " + instance.getAttributeValue("name"));
+            return true;
+        }
+        return false;
+    }
+
+    public Collection<ModelElementInstance> getElementsOfType(Class<? extends ModelElementInstance> c) {
         ModelElementType taskType = modelInstance.getModel().getType(c);
         return modelInstance.getModelElementsByType(taskType);
-    }
-
-    // Find the following flow nodes of a task or a gateway
-    public static Collection<FlowNode> getFlowingFlowNodes(FlowNode node) {
-        Collection<FlowNode> followingFlowNodes = new ArrayList<>();
-        for (SequenceFlow sequenceFlow : node.getOutgoing()) {
-            followingFlowNodes.add(sequenceFlow.getTarget());
-        }
-        return followingFlowNodes;
-    }
-
-    public static Collection<FlowNode> getAllFlowNodes(FlowNode source) {
-        Collection<FlowNode> allFlowNodes = new ArrayList<>();
-        Collection<FlowNode> flowNodes = getFlowingFlowNodes(source);
-        for (FlowNode flowNode : flowNodes) {
-            System.out.println(flowNode.getName());
-        }
-        return allFlowNodes;
     }
 }
