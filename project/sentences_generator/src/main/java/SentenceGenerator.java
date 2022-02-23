7301c494bada9612d7c0d44ea1505edc4f366f0d
guillempla <guillem.pla@est.fib.upc.edu>
2022-02-23 18:59:45 +0100

    Added SentenceGenerator

---
+++ project/sentences_generator/src/main/java/SentenceGenerator.java
@@ -0,0 +1,11 @@
+public class SentenceGenerator {
+    String originalSentence;
+
+    public SentenceGenerator(String originalSentence) {
+        this.originalSentence = originalSentence;
+    }
+
+    public String getFinalSentence() {
+        return "";
+    }
+}
