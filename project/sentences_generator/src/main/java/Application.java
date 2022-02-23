1c12b2a8f4c274e8f165b33d0d4a750d389dc80b
guillempla <guillem.pla@est.fib.upc.edu>
2022-02-23 19:09:27 +0100

    Added getJSONPaths

--- project/sentences_generator/src/main/java/Application.java
+++ project/sentences_generator/src/main/java/Application.java
@@ -1,8 +1,32 @@
-import simplenlg.framework.*;
-import simplenlg.lexicon.*;
-import simplenlg.realiser.english.*;
+import java.io.File;
+import java.util.ArrayList;
 
 public class Application {
+
     public static void main(String[] args) {
+        String responsesPath = "../freeling_responses/";
+        if (args.length > 0) {
+            responsesPath = args[0];
+        }
+
+        ArrayList<String> json_paths = getJSONPaths(responsesPath);
+        System.out.println(json_paths);
+    }
+
+    private static ArrayList<String> getJSONPaths(String path) {
+        ArrayList<String> paths = new ArrayList<>();
+
+        File directoryPath = new File(path);
+        //List of all files and directories
+        File[] filesList = directoryPath.listFiles();
+        if (filesList != null) {
+            for (File file : filesList) {
+
+                File filePath = new File(file.getAbsolutePath());
+                //List of all files and directories
+                paths.add(file.getAbsolutePath());
+            }
+        }
+        return paths;
     }
 }
