e57bec18efc188cfaccf0074c86515e076c25ef4
guillempla <guillem.pla@est.fib.upc.edu>
2022-02-23 18:53:56 +0100

    Added class Sentence

---
+++ project/sentences_generator/src/main/java/Sentence.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class Sentence {
+    String originalSentence;
+    String finalSentence;
+    ArrayList<String> actions;
+
+    public Sentence(String originalSentence, String finalSentence, ArrayList<String> actions) {
+        this.originalSentence = originalSentence;
+        this.finalSentence = finalSentence;
+        this.actions = actions;
+    }
+
+    public String getOriginalSentence() {
+        return originalSentence;
+    }
+
+    public String getFinalSentence() {
+        return finalSentence;
+    }
+
+    public ArrayList<String> getActions() {
+        return actions;
+    }
+
+    public void setOriginalSentence(String originalSentence) {
+        this.originalSentence = originalSentence;
+    }
+
+    public void setFinalSentence(String finalSentence) {
+        this.finalSentence = finalSentence;
+    }
+
+    public void setActions(ArrayList<String> actions) {
+        this.actions = actions;
+    }
+}
