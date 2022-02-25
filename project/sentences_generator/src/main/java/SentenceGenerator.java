6655a2fdd988f8b9331f3288e61fd2610cec72b6
guillempla <guillem.pla@est.fib.upc.edu>
2022-02-25 20:29:55 +0100

    Added simpleNLG attributes

--- project/sentences_generator/src/main/java/SentenceGenerator.java
+++ project/sentences_generator/src/main/java/SentenceGenerator.java
@@ -7,12 +7,20 @@ import simplenlg.features.*;
 import java.util.Map;
 
 public class SentenceGenerator {
+    Lexicon lexicon;
+    NLGFactory nlgFactory;
+    Realiser realiser;
+
     String originalSentence;
     String lane;
     Map<String, String> actions;
     String finalSentence;
 
     public SentenceGenerator(String originalSentence, String lane, Map<String, String> actions) {
+        this.lexicon = Lexicon.getDefaultLexicon();
+        this.nlgFactory = new NLGFactory(lexicon);
+        this.realiser = new Realiser(lexicon);
+
         this.originalSentence = originalSentence;
         this.lane = lane;
         this.actions = actions;
