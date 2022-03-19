ae967ca883008b013cc357595a4247277fe4361f
guillempla <guillem.pla@est.fib.upc.edu>
2022-03-19 15:47:02 +0100

    Added retrieveElements

--- project/sentences_joiner/src/main/java/JSONReader.java
+++ project/sentences_joiner/src/main/java/JSONReader.java
@@ -1,12 +1,12 @@
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import simplenlg.phrasespec.SPhraseSpec;
+
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import simplenlg.framework.NLGElement;
 
 public class JSONReader {
     public static final String GENERATED_PATH = "../sentences_joined/";
@@ -14,20 +14,60 @@ public class JSONReader {
     String fileName;
     String path;
 
-    ArrayList<NLGElement> finalPhrases;
     JSONObject jsonElements;
+    ArrayList<ElementVertex> vertexElements;
 
     public JSONReader(String fileName, String path) {
         this.fileName = fileName;
         this.path = path;
         this.jsonElements = parseJSON(this.path);
+//        testJBPT();
+        this.vertexElements = this.retrieveElements();
+    }
+
+    private ArrayList<ElementVertex> retrieveElements() {
+        ArrayList<ElementVertex> elements = new ArrayList<>();
+
         for (Object key : jsonElements.keySet()) {
             JSONObject jsonElement = (JSONObject) jsonElements.get(key);
-            System.out.println(jsonElement.get("type"));
+            String id = jsonElement.get("id").toString();
+            String sentence = jsonElement.get("finalSentence").toString();
+            SPhraseSpec phrase = retrievePhrase((JSONObject) jsonElement.get("finalPhrase"));
+            String type = jsonElement.get("type").toString();
+            ElementVertex vertex = new ElementVertex(id, sentence, phrase, type);
 
-            System.out.println();
+            elements.add(vertex);
         }
+
+        return elements;
     }
+
+    private SPhraseSpec retrievePhrase(JSONObject jsonElement) {
+        return null;
+    }
+
+//    private void testJBPT() {
+//        MultiDirectedGraph g = new MultiDirectedGraph();
+//
+//        Vertex s = new Vertex("s");
+//        Vertex u = new Vertex("u");
+//        Vertex v = new Vertex("v");
+//        Vertex w = new Vertex("w");
+//        Vertex x = new Vertex("x");
+//        Vertex t = new Vertex("t");
+//
+//        g.addEdge(s,u);
+//        g.addEdge(u,v);
+//        g.addEdge(v,x);
+//        g.addEdge(u,w);
+//        g.addEdge(w,x);
+//        g.addEdge(v,w);
+//        g.addEdge(x,t);
+//
+//        RPST<DirectedEdge,Vertex> rpst = new RPST<DirectedEdge,Vertex>(g);
+//        System.out.println(rpst);
+//        System.out.println("test");
+//    }
 
     private JSONObject parseJSON(String path) {
         JSONParser parser = new JSONParser();
