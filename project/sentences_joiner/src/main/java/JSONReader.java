77e2724c58b273d433afb7d557dd426d6225ad48
guillempla <guillem.pla@est.fib.upc.edu>
2022-03-19 20:42:14 +0100

    Added class to retrive NLGElement phrase

--- project/sentences_joiner/src/main/java/JSONReader.java
+++ project/sentences_joiner/src/main/java/JSONReader.java
@@ -1,13 +1,12 @@
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import simplenlg.phrasespec.SPhraseSpec;
+import simplenlg.framework.NLGElement;
 
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Objects;
 
 public class JSONReader {
     public static final String GENERATED_PATH = "../sentences_joined/";
@@ -31,10 +30,15 @@ public class JSONReader {
 
         for (Object key : jsonElements.keySet()) {
             JSONObject jsonElement = (JSONObject) jsonElements.get(key);
-            String id = getStringFromJSON(jsonElement, "id");
-            String sentence = getStringFromJSON(jsonElement, "finalSentence");
-            SPhraseSpec phrase = retrievePhrase((JSONObject) jsonElement.get("finalPhrase"));
-            String type = getStringFromJSON(jsonElement, "type");
+
+            String id = jsonElement.get("id").toString();
+            String sentence = jsonElement.get("finalSentence").toString();
+            String type = jsonElement.get("type").toString();
+
+            String originalSentence = jsonElement.get("name").toString();
+            JSONObject finalPhraseJSON = (JSONObject) jsonElement.get("finalPhrase");
+            NLGElement phrase = retrievePhrase(originalSentence, finalPhraseJSON);
+
             ElementVertex vertex = new ElementVertex(id, sentence, phrase, type);
 
             elements.add(vertex);
@@ -43,25 +47,9 @@ public class JSONReader {
         return elements;
     }
 
-    private SPhraseSpec retrievePhrase(JSONObject jsonElement) {
-        Boolean objectDeterminant = getBooleanFromJSON(jsonElement, "objectDeterminant");
-        Boolean interrogative = getBooleanFromJSON(jsonElement, "interrogative");
-        Boolean objectPlural = getBooleanFromJSON(jsonElement, "objectPlural");
-        Boolean existActions = getBooleanFromJSON(jsonElement, "existsActions");
-        String subject = getStringFromJSON(jsonElement, "subject");
-        String verb = getStringFromJSON(jsonElement, "verb");
-        String complement = getStringFromJSON(jsonElement, "complement");
-        String object = getStringFromJSON(jsonElement, "object");
-
-        return null;
-    }
-
-    private String getStringFromJSON(JSONObject jsonElement, String target) {
-        return jsonElement.get(target).toString();
-    }
-
-    private Boolean getBooleanFromJSON(JSONObject jsonElement, String target) {
-        return Objects.equals(jsonElement.get(target).toString(), "true");
+    private NLGElement retrievePhrase(String originalSentence, JSONObject jsonElement) {
+        PhraseRetriever retriever = new PhraseRetriever(originalSentence, jsonElement);
+        return retriever.getPhrase();
     }
 
 //    private void testJBPT() {
