39cc4687b0a9489bbd5a462817546c2586fe5a31
guillempla <guillem.pla@est.fib.upc.edu>
2022-03-19 14:35:56 +0100

    Updated ElementVertex

--- project/sentences_joiner/src/main/java/ElementVertex.java
+++ project/sentences_joiner/src/main/java/ElementVertex.java
@@ -2,6 +2,7 @@ import org.jbpt.hypergraph.abs.Vertex;
 import simplenlg.phrasespec.SPhraseSpec;
 
 public class ElementVertex extends Vertex {
+    // Inspired in https://github.com/pawelgalka/pm_split_miner/blob/5c140f1d31303a5afc0337c481c80568f10d6416/java-joiner/src/main/java/pl/edu/agh/Graph.java
     private final String sentence;
     private final SPhraseSpec phrase;
     private final String type;
