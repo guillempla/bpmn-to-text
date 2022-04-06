618646da08d62e4319393319686080f2ba4114bf
guillempla <guillem.pla@est.fib.upc.edu>
2022-04-06 20:30:20 +0200

    Removed debug print

--- project/sentences_joiner/src/main/java/PhraseRetriever.java
+++ project/sentences_joiner/src/main/java/PhraseRetriever.java
@@ -34,7 +34,6 @@ public class PhraseRetriever {
 
         this.initializePhraseAttributes(jsonElement);
         this.phrase = this.retrievePhrase();
-        System.out.println("    " + realiser.realiseSentence(this.phrase));
     }
 
     public NLGElement getPhrase() {
