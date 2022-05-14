dada37ac00dc40c9c3c91c836e683d8c8c631014
guillempla <guillem.pla@est.fib.upc.edu>
2022-05-14 17:38:22 +0200

    Added Final class

---
+++ project/sentences_joiner/src/main/java/Final.java
@@ -0,0 +1,25 @@
+import simplenlg.framework.NLGElement;
+
+import java.util.Collection;
+
+public class Final extends Connector {
+    public Final() {
+        super(java.util.List.of("Finally ", "To conclude "));
+    }
+
+    public Final(String connector) {
+        super(connector);
+    }
+
+    public Final(Collection<String> connectors) {
+        super(connectors);
+    }
+
+    @Override
+    public void transformSentenceWithConnector(Sentence sentence) {
+        String sentenceString = sentence.paragraphToString();
+        sentenceString = selectedConnector + sentenceString;
+        NLGElement branchPhrase = nlgFactory.createSentence(sentenceString);
+        sentence.setCoordinatedPhrase(branchPhrase);
+    }
+}
