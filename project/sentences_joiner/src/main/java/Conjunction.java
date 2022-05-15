7fadbc45c2652beda0492dae2e7004b1651ea856
guillempla <guillem.pla@est.fib.upc.edu>
2022-05-15 18:33:11 +0200

    Added Conjunction class

---
+++ project/sentences_joiner/src/main/java/Conjunction.java
@@ -0,0 +1,25 @@
+import simplenlg.framework.NLGElement;
+
+import java.util.Collection;
+
+public class Conjunction extends Connector {
+    public Conjunction() {
+        super(java.util.List.of("then", "after", "and", "later", "next", "after that"));
+    }
+
+    public Conjunction(String connector) {
+        super(connector);
+    }
+
+    public Conjunction(Collection<String> connectors) {
+        super(connectors);
+    }
+
+    @Override
+    public void transformSentenceWithConnector(Sentence sentence) {
+        String sentenceString = sentence.paragraphToString();
+        NLGElement branchPhrase = nlgFactory.createSentence(sentenceString);
+        sentence.setConjunction(selectedConnector);
+        sentence.setCoordinatedPhrase(branchPhrase);
+    }
+}
